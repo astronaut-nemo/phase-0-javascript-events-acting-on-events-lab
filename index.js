66af926967e2f66eb1cfe9d525bf474c48ac8353
Joanna <astronautnemo@gmail.com>
2022-11-20 13:55:00 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Your code here
+
+const dodger = document.getElementById('dodger');
+
+// Dodger Left Movement
+function moveDodgerLeft(){
+    const leftNumbers = dodger.style.left.replace('px', '');
+    const left = parseInt(leftNumbers, 10);
+
+    if(left > 0){
+        dodger.style.left = `${left - 1}px`;
+    }
+}
+
+// Dodger Right Movement
+function moveDodgerRight(){
+    const leftNumbers = dodger.style.left.replace('px','');
+    const left = parseInt(leftNumbers, 10);
+    
+    if(left < 360){
+        dodger.style.left = `${left + 1}px`
+    }
+}
+
+// Event Listener
+document.addEventListener('keydown', function(e){
+    if(e.key === "ArrowLeft"){
+        moveDodgerLeft();
+    }
+    else if(e.key === "ArrowRight"){
+        moveDodgerRight();
+    }
+});
